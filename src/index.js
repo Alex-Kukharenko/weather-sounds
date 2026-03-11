de0fc70a200061ad2671bec987e4a377b163f86e
Alex-Kukharenko <alexkox@mail.ru>
2026-03-11 22:45:01 +1100

    feat(rangeInput.js) создана логика rangeInput.js
    Создана логика rangeInput.js
    изменены:
    public/index.html
    src/index.js
    src/style.scss
    добавлено:
    src/js/rangeInput.js
    
    https://lk.result-university.com/pl/teach/control/lesson/view?id=344882729&editMode=0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.scss'
+import { setRangeColor } from './js/rangeInput.js'
+
+const colorRangeInput = [
+  '#34badb',
+  '#ddd',
+  '#34badb',
+]
+
+const slider = document.querySelector('.volume-menu__input')
+slider.addEventListener('input', () => {
+  setRangeColor(slider, ...colorRangeInput);
+});
+
+
+setRangeColor(slider, ...colorRangeInput);
+
+
