1febdcdbb31eb075b6c8c71787d7c5bb19f9cd8e
Alex Kukharenko <alexkox@mail.ru>
2026-03-13 17:19:22 +1100

    implement basic audio player logic

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
+  eslintConfigPrettier, // Отключает конфликтующие с Prettier правила
+  {
+    rules: {
+      'prefer-const': 'error',
+      'no-var': 'error',
+    },
+  },
+])
+
--- src/index.js
+++ src/index.js
@@ -1,47 +1,79 @@
 import './style.scss'
-import { data } from './js/data.js';
+import { data } from './js/data.js'
 import { setRangeColor } from './js/rangeInput.js'
 
-const colorRangeInput = [
-  '#34badb',
-  '#ddd',
-  '#34badb',
-]
-
-function input() {
+const colorRangeInput = ['#34badb', '#ddd', '#34badb']
+let currentAudio = null
+function createRangeInput() {
   const input = document.createElement('input')
-  input.type = 'range';
-  input.min = 0;
-  input.max = 100;
-  input.value = 50;
-  input.step = 1;
+  input.type = 'range'
+  input.min = 0
+  input.max = 100
+  input.value = 50
+  input.step = 1
   input.className = 'volume__input'
+
   setRangeColor(input, ...colorRangeInput)
+
   input.addEventListener('input', () => {
     setRangeColor(input, ...colorRangeInput)
+    const volume = input.value / 100
+    if (currentAudio) {
+      currentAudio.volume = volume
+    }
   })
+  input.getVolume = () => input.value / 100
   return input
 }
 
+const audioMap = {}
+
 const handleButtonClick = (data) => {
   const container = document.querySelector('.container')
-  const iconPause = event.target.lastElementChild
-  console.log(iconPause)
+  const iconPause = event.target.lastChild
   container.style.setProperty('--before-bg', `url('${data.bgImg}')`)
-  const musicPlay = false
-  console.log(musicPlay);
+
+  const src = data.musicSrc
+
+  // создаём Audio только один раз для каждого src
+  if (!audioMap[src]) {
+    audioMap[src] = new Audio(src)
+  }
+  const audio = audioMap[src]
+  document.querySelectorAll('.button-menu__icon-paused').forEach((icon) => {
+    icon.style.setProperty('--opacity-btn', 0) // или убираем класс .paused
+  })
+  // если уже играет эта же мелодия – пауза
+  if (currentAudio === audio && !audio.paused) {
+    audio.pause()
+    iconPause.style.setProperty('--opacity-btn', 1)
+    return
+  }
+
+  // если играла другая – остановить её
+  if (currentAudio && currentAudio !== audio) {
+    currentAudio.pause()
+    iconPause.style.setProperty('--opacity-btn', 0)
+    currentAudio.currentTime = 0 // по желанию
+  }
+
+  currentAudio = audio
+  if (volumeInput) {
+    audio.volume = volumeInput.value / 100
+  }
+  audio.play()
 }
 
 function createButtonMenu(data) {
-  const buttonMenuWrap = document.createElement('div')
-  buttonMenuWrap.className = 'button-menu'
+  const buttonMenu = document.createElement('div')
+  buttonMenu.className = 'button-menu'
 
   data.forEach((item) => {
     const button = document.createElement('button')
     button.className = 'button-menu__play'
     button.style.backgroundImage = item.bgImg ? `url('${item.bgImg}')` : 'none'
     const iconPause = document.createElement('img')
-    iconPause.className = 'button-menu__icon-pause'
+    iconPause.className = 'button-menu__icon-paused'
     iconPause.src = './assets/icons/pause.svg'
     iconPause.alt = 'icon'
     if (item.iconSrc) {
@@ -56,24 +88,24 @@ function createButtonMenu(data) {
       handleButtonClick(item)
     })
 
-    buttonMenuWrap.appendChild(button)
+    buttonMenu.appendChild(button)
   })
 
-  return buttonMenuWrap
+  return buttonMenu
 }
-
+let volumeInput = null
 function app(data) {
   const container = document.createElement('div')
   container.className = 'container'
   const title = document.createElement('h1')
   title.className = 'title'
   title.innerText = 'Whether sound'
+  volumeInput = createRangeInput()
   container.append(title)
   container.append(createButtonMenu(data))
-  container.append(input())
+  container.append(volumeInput)
   return container
 }
 
 const rootElement = document.getElementById('app')
 rootElement.append(app(data))
-
